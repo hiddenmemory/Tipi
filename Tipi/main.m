6d7d6bd15d1e3fa9240f765541710b74b97d868a
Chris Ross <chris@darkrock.co.uk>
2012-04-05 11:50:55 +0100

    Initial Commit

---
+++ Tipi/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Tipi
+//
+//  Created by Chris Ross on 05/04/2012.
+//  Copyright (c) 2012 hiddenMemory Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
