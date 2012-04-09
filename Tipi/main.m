ce98fbe18ab21b974d36fb48613f35654ae6266a
Chris Ross <chris@hiddenmemory.co.uk>
2012-04-09 15:18:30 +0100

    Turn the main application into a test suite app

--- Tipi/main.m
+++ Tipi/main.m
@@ -11,48 +11,55 @@
 #import "Tipi.h"
 
 int main(int argc, const char * argv[]) {
-
+	
 	@autoreleasepool {
-		NSMutableDictionary *environment = [NSMutableDictionary dictionary];
-		
-		[environment setObject:[^NSString*( TPTemplateNode *node, NSMutableDictionary *environment ) {
-			TPMarkdownDataParser *parser = [TPMarkdownDataParser parserForFile:[node.values objectAtIndex:0]];
+		if( argc > 1 ) {
+			NSString *path = [NSString stringWithFormat:@"%s/Tests/", argv[1]];
 			
-			// Duplicate the environment for implementing the child nodes
-			NSMutableDictionary *invokeEnvironment = [NSMutableDictionary dictionaryWithDictionary:environment];
+			NSLog(@"Scanning %@ for test suites...", path);
 			
-			// Add the key-values from the data file
-			[invokeEnvironment addEntriesFromDictionary:parser.values];
-			
-			return [node.childNodes tp_templateNodesExpandedUsingEnvironment:invokeEnvironment];
-		} copy] forKey:@"include"];
-		
-		for( int i = 1; i <= 6; i++ ) {
-			NSString *inputPath = [NSString stringWithFormat:@"/Users/chris/Repositories/git/hiddenMemory/Tipi/Tests/basic/Test0%d.input", i];
-			NSString *outputPath = [NSString stringWithFormat:@"/Users/chris/Repositories/git/hiddenMemory/Tipi/Tests/basic/Test0%d.output", i];
-			
-			TPTemplateParser *q = [TPTemplateParser parserForFile:inputPath];
-			NSString *expansion = [q expansionUsingEnvironment:[NSDictionary dictionary]];
-								   
-			if( [[NSFileManager defaultManager] fileExistsAtPath:outputPath] ) {
-				NSString *goldenExpansion = [NSString stringWithContentsOfFile:outputPath
-																	  encoding:NSUTF8StringEncoding
-																		 error:nil];
+			[[[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:nil] enumerateObjectsUsingBlock:^(NSString *suiteName, NSUInteger idx, BOOL *stop) {
+				BOOL isDirectory = NO;
+				NSString *testPath = [NSString stringWithFormat:@"%@%@/", path, suiteName];
 				
-				if( [goldenExpansion isEqualToString:expansion] ) {
-					NSLog(@"Test %d passed", i);
+				if( [[NSFileManager defaultManager] fileExistsAtPath:testPath isDirectory:&isDirectory] && isDirectory ) {
+					NSLog(@"Test suite: %@ (path: %@)", suiteName, testPath);
+					
+					[[[NSFileManager defaultManager] contentsOfDirectoryAtPath:testPath error:nil] enumerateObjectsUsingBlock:^(NSString *item, NSUInteger idx, BOOL *stop) {
+						if( [item hasSuffix:@".input"] ) {
+							NSString *inputPath = [NSString stringWithFormat:@"%@%@", testPath, item];
+							NSString *outputPath = [NSString stringWithFormat:@"%@%@", testPath, [item stringByReplacingOccurrencesOfString:@".input"
+																																 withString:@".output"]];
+							
+							TPTemplateParser *q = [TPTemplateParser parserForFile:inputPath];
+							NSString *expansion = [q expansionUsingEnvironment:[NSDictionary dictionary]];
+							
+							if( [[NSFileManager defaultManager] fileExistsAtPath:outputPath] ) {
+								NSString *goldenExpansion = [NSString stringWithContentsOfFile:outputPath
+																					  encoding:NSUTF8StringEncoding
+																						 error:nil];
+								
+								if( [goldenExpansion isEqualToString:expansion] ) {
+									NSLog(@"[%@] Test Passed", item);
+								}
+								else {
+									NSLog(@"[%@] >>>> Test failed <<<<", item);
+								}
+							}
+							else {
+								NSLog(@"[%@] Creating new test output:\n%@", item, expansion);
+								[expansion writeToFile:outputPath
+											atomically:NO
+											  encoding:NSUTF8StringEncoding
+												 error:nil];
+							}
+						}
+					}];
 				}
-				else {
-					NSLog(@">>>> Test %d failed <<<<", i);
-				}
-			}
-			else {
-				NSLog(@"Expansion:\nSTART:\n%@:END", expansion);
-				[expansion writeToFile:outputPath
-							atomically:NO
-							  encoding:NSUTF8StringEncoding
-								 error:nil];
-			}
+			}];
+		}
+		else {
+			NSLog(@"Please provide a path to the Tipi test suite");
 		}
 	}
     return 0;
